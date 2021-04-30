2f8931cbe8da6946b3920a75651fffefe60b0d8f
Vishwas Puri <vishwaspuri09@gmail.com>
2021-05-01 03:11:17 +0530

    Simple Payment checker without forms

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -6,22 +6,37 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import random
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ActionPaymentStatus(Action):
+
+    def name(self) -> Text:
+        return "action_payment_status"
+
+    def run(self,   dispatcher, tracker: Tracker,domain: Dict[Text, Any],) -> List[Dict[Text, Any]]:
+        if random.uniform(0, 1) > 0.5:
+            dispatcher.utter_message("The payment is complete")
+        else:
+            dispatcher.utter_message("The payment is not complete")
+
+        return []
