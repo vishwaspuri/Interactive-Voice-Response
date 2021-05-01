d9153ec57b94f0e5fc0b17d91fe7234aa42f09d8
Vishwas Puri <vishwaspuri09@gmail.com>
2021-05-01 19:07:14 +0530

    Create appointment

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -28,19 +28,6 @@ class ActionHelloWorld(Action):
         return []
 
 
-# class ActionPaymentStatus(Action):
-
-#     def name(self) -> Text:
-#         return "action_payment_status"
-
-#     def run(self,   dispatcher, tracker: Tracker,domain: Dict[Text, Any],) -> List[Dict[Text, Any]]:
-        # if random.uniform(0, 1) > 0.5:
-        #     dispatcher.utter_message("The payment is complete")
-        # else:
-        #     dispatcher.utter_message("The payment is not complete")
-
-#         return []
-
 class ValidatePaymentVerificationForm(Action):
     def name(self) -> Text:
         return "payment_id_form"
@@ -55,12 +42,43 @@ class ValidatePaymentVerificationForm(Action):
             
         return [SlotSet("requested_slot"), None]
 
-class PaymentStatus(Action):
+class ActionPaymentStatus(Action):
     def name(self) -> Text:
         return  "action_payment_status"
 
     def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         if random.uniform(0, 1) > 0.5:
             dispatcher.utter_message(template="utter_payment_status", Payment_status="done", Payment_id=tracker.get_slot("payment_id"))
+            return []
         else:
             dispatcher.utter_message(template="utter_payment_status", Payment_status="not done", Payment_id=tracker.get_slot("payment_id"))
+            return []
+
+class ValidateAppointmentForm(Action):
+    def name(self) -> Text:
+        return "appointment_form"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict) -> List[EventType]:
+        required_slots = ["user_name", "appointment_with", "appointment_time"]
+        if tracker.get_slot(slot_name) is None:
+                return [SlotSet("requested_slot", slot_name)]
+        
+        return [SlotSet("requested_slot"), None]
+
+
+
+
+class ActionCreateMeeting(Action):
+    def name(self)  -> Text:
+        return "action_create_appointment"
+    
+    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        # user_name is the name of the person making the appointment
+        User_name = tracker.get_slot("user_name")
+        # appointment_with is the name of the person with whom the appointment is being made
+        appointment_with = tracker.get_slot("appointment_with")
+        # appointment_time is the time of the appointment
+        appointment_time = tracker.get_slot("appointment_time")
+
+        dispatcher.utter_message(template="utter_appointment_confirmed", User_name=user_name, Appointment_with=appointment_with, Appointment_time=appointment_time)
+
+
